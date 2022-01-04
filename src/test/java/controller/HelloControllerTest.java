71d8bc837bd71941123c51b496db423605d5f6f8
VINTO1819 <develretr0@gmail.com>
2022-01-04 12:17:14 +0900

    🧪:: (#5) DTO 테스트코드 추가

--- src/test/java/controller/HelloControllerTest.java
+++ src/test/java/controller/HelloControllerTest.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.example.springbootstudy.Application;
 import org.example.springbootstudy.controller.HelloController;
+import org.example.springbootstudy.domain.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -30,5 +32,19 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(expected));
     }
+
+    @Test
+    public void helloDtoTest() throws Exception {
+        String name = "테스트";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(
+                name, amount
+        );
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
 
 }
