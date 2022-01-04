bf593b89a9146e58127f922e545ff57c9d934e3a
V!NT0 <develretr0@gmail.com>
2022-01-04 13:10:31 +0900

    🔀:: master로 Feature/#5 병합
    * 🖋️:: (#3) hello를 반환하는 컨트롤러 생성
    
    * 🧪:: (#3) merge용 테스트 코드 작성
    
    * ⚙️:: (#5) lombok을 gradle에 추가
    
    * 💥:: (#5) lombok으로 작성된 첫번째 DTO
    
    * ⚙️:: (#5) gradle에 junit 종속성 추가
    
    * ♻️:: (#5) member를 private으로 변경
    
    * 🧪:: (#5) DTO 테스트코드 추가
    
    * 🖋️:: (#5) dto를 받는 함수 생성
    
    * 🧪:: (#5) dto 함수에 대한 테스트코드 작성

--- src/test/java/controller/HelloControllerTest.java
+++ src/test/java/controller/HelloControllerTest.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.example.springbootstudy.Application;
 import org.example.springbootstudy.controller.HelloController;
+import org.example.springbootstudy.domain.dto.HelloResponseDto;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -31,4 +37,16 @@ public class HelloControllerTest {
                 .andExpect(content().string(expected));
     }
 
+    @Test
+    public void helloDtoTest() throws Exception {
+        String name = "테스트";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.is(name)))
+                .andExpect(jsonPath("$.amount", Matchers.is(amount)));
+    }
 }
