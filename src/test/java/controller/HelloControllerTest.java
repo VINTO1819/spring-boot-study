c8cc971d171b26000e454392896b41d8d9f34385
V!NT0 <develretr0@gmail.com>
2022-01-09 14:46:32 +0900

    🔀:: (#11) 패키지 구조 변경(+ 누락 커밋)
    * ♻️:: (#11) 누락된 폴더삭제 커밋 적용

--- src/test/java/controller/HelloControllerTest.java
+++ src/test/java/controller/HelloControllerTest.java
@@ -1,21 +1,16 @@
 package controller;
 
 import org.example.springbootstudy.Application;
-import org.example.springbootstudy.controller.HelloController;
-import org.example.springbootstudy.domain.dto.HelloResponseDto;
+import org.example.springbootstudy.domain.hello.presentation.HelloController;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
