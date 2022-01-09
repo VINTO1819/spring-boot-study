cffd76d7e6430f647f44b6efbd2d214ce98be83e
VINTO1819 <develretr0@gmail.com>
2022-01-09 12:26:40 +0900

    🖋️:: (#9) 포스트 게시 기능 추가

--- src/main/java/org/example/springbootstudy/controller/PostController.java
+++ src/main/java/org/example/springbootstudy/controller/PostController.java
@@ -1,11 +1,23 @@
 package org.example.springbootstudy.controller;
 
+import lombok.AllArgsConstructor;
+import org.example.springbootstudy.domain.dto.CreateNewPostRequestDto;
+import org.example.springbootstudy.domain.dto.CreateNewPostResponseDto;
+import org.example.springbootstudy.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("posts")
 public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public CreateNewPostResponseDto createNew(CreateNewPostRequestDto dto) {
+        return postService.createNew(dto);
+    }
 
 }
--- src/main/java/org/example/springbootstudy/service/PostService.java
+++ src/main/java/org/example/springbootstudy/service/PostService.java
@@ -1,5 +1,22 @@
 package org.example.springbootstudy.service;
 
+import lombok.AllArgsConstructor;
+import org.example.springbootstudy.domain.dto.*;
+import org.example.springbootstudy.domain.entity.PostEntity;
+import org.example.springbootstudy.domain.repository.PostRepository;
+import org.example.springbootstudy.facade.PostFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+    private final PostFacade postFacade;
+
+    public CreateNewPostResponseDto createNew(CreateNewPostRequestDto dto) {
+        PostEntity savedEntity = postRepository.save(dto.toEntity());
+        return CreateNewPostResponseDto.fromEntity(savedEntity);
+    }
 
 }
